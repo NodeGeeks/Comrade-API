cbdd62323c39a774b869e07fafa13f4182b40912
aaronrussell <aaron@teknologenie.com>
2015-03-01 22:17:28 -0800

    Cleaned up UsersController code and setup development environment more

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -11,7 +11,9 @@ module.exports = {
         Users.find({id: req.body.id, accessToken: req.body.token}).exec(function (err, user) {
             if (user.length >= 1) {
                 res.send(true);
-            } else {res.send(false)}
+            } else {
+                res.send(false);
+            }
         });
     },
     login: function (req, res) {
@@ -20,21 +22,29 @@ module.exports = {
         var thingToEncrypt = "comrade" + _.random(598, 78905478) + req.body + _.random(23, 8300000000);
         var emailRegex = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
 
-        if (emailRegex.test(req.body.email) == true) {
+        if (emailRegex.test(req.body.email)) {
             Users.findOne().where({email: req.body.email})
             .then(function (user) {
                 bcrypt.compare(req.body.password, user.password, function (err, match) {
-                    if (err) res.serverError({ error: 'Server error' }, 500);
+
+                    if (err) {
+                        res.serverError( {
+                            error: 'Server error'
+                        }, 500);
+                    }
 
                     if (match) {
                         req.session.user = user.id;
                         bcrypt.genSalt(10, function(err, salt) {
-                            if (err) return next(err);
+                            if (err) {
+                                return next(err);
+                            }
 
                             bcrypt.hash(thingToEncrypt, salt, function () {}, function (err, hash) {
-                                if (err) return next(err);
-                                var accessToken = hash;
-                                Users.update({id: user.id}, {accessToken: accessToken})
+                                if (err) {
+                                    return next(err);
+                                }
+                                Users.update( {id: user.id}, {accessToken: hash})
                                 .then(function (updated) {
                                     return res.json(updated);
                                 })
@@ -44,8 +54,12 @@ module.exports = {
                             });
                         });
                     } else {
-                        if (req.session.user) req.session.user = null;
-                        res.serverError({ error: 'Invalid password' }, 401);
+                        if (req.session.user) {
+                            req.session.user = null;
+                        }
+                        res.serverError( {
+                            error: 'Invalid password'
+                        }, 401);
                     }
                 });
             })
@@ -56,17 +70,22 @@ module.exports = {
             Users.findOne().where({comradeUsername: req.body.email})
                 .then(function (user) {
                     bcrypt.compare(req.body.password, user.password, function (err, match) {
-                        if (err) res.serverError({ error: 'Server error' }, 500);
+                        if (err) {
+                            res.serverError({ error: 'Server error' }, 500);
+                        }
 
                         if (match) {
                             req.session.user = user.id;
                             bcrypt.genSalt(10, function(err, salt) {
-                                if (err) return next(err);
+                                if (err) {
+                                    return next(err);
+                                }
 
                                 bcrypt.hash(thingToEncrypt, salt, function () {}, function (err, hash) {
-                                    if (err) return next(err);
-                                    var accessToken = hash;
-                                    Users.update({id: user.id}, {accessToken: accessToken})
+                                    if (err) {
+                                        return next(err);
+                                    }
+                                    Users.update({id: user.id}, {accessToken: hash})
                                         .then(function (updated) {
                                             return res.json(updated);
                                         })
@@ -76,7 +95,9 @@ module.exports = {
                                 });
                             });
                         } else {
-                            if (req.session.user) req.session.user = null;
+                            if (req.session.user) {
+                                req.session.user = null;
+                            }
                             res.serverError({ error: 'Invalid password' }, 401);
                         }
                     });
@@ -134,19 +155,19 @@ module.exports = {
                         Users.find({comradeUsername: req.body.username}).exec( function findCB(err, found) {
 
                             if (found.length >= 1) {
-                                res.json({exists: 'this username already exists', errorCode: 'USERNAME_EXISTS'})
-                            } else if (found ==  null || found == undefined || found.length < 1) {
+                                res.json({exists: 'this username already exists', errorCode: 'USERNAME_EXISTS'});
+                            } else if (!found || found.length < 1) {
                                 Users.find({email: req.body.email}).exec( function findCB(err, found) {
                                     if (found.length >= 1) {
-                                        res.json({exists: 'this email already exists', errorCode: 'EMAIL_EXISTS'})
-                                    } else if (found ==  null || found == undefined || found.length < 1) {
+                                        res.json({exists: 'this email already exists', errorCode: 'EMAIL_EXISTS'});
+                                    } else if (!found || found.length < 1) {
                                         Users.create({ comradeUsername: req.body.username, firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, password: req.body.password, accessToken: req.body.accessToken, activationToken: req.body.activationToken}).exec(function aftwards(err, user) {
                                             if (user) {
                                                 console.log('this is the first time this user has been created ', user);
                                                 var mailOptions = {
                                                     from: 'aaron@teknologenie.com',
                                                     to: req.body.email,
-                                                    subject: 'Comrade Account Verficiation',
+                                                    subject: 'Comrade Account Verification',
                                                     text: 'In order to use your Comrade account please follow the link bellow to verify your email address and activate your account /n /n https://comradeapp.com/users/activate?email='+req.body.email+'&activationToken='+hash1+''
                                                 };
                                                 var nodemailer = require("nodemailer");
@@ -182,7 +203,7 @@ module.exports = {
                                     if (err) {
                                         res.json({ error: 'Could not create hash' }, 404);
                                     }
-                                })
+                                });
                             }
                             if (err) {
                                 res.json(err);
@@ -206,166 +227,105 @@ module.exports = {
     },
 
     loginSocialAccount: function (req, res) {
-        var provider = req.body.provider;
-        var socialID = req.body.socialID;
-        var firstName = req.body.firstName;
-        var lastName = req.body.lastName;
-        var accessToken = "invalidFromLogin";
-        var bcrypt = require('bcrypt-nodejs');
-        var thingToEncrypt = "comrade" + _.random(598, 78905478) + socialID + _.random(23, 8300000000);
+        var provider = req.body.provider,
+            socialID = req.body.socialID,
+            firstName = req.body.firstName,
+            lastName = req.body.lastName,
+            accessToken = 'invalidFromLogin',
+            bcrypt = require('bcrypt-nodejs'),
+            thingToEncrypt = 'comrade' + _.random(598, 78905478) + socialID + _.random(23, 8300000000),
+            socialObject, socialUserObject;
+
+        switch (provider){
+            case 'facebook':
+                socialObject = { facebookID: socialID };
+                socialUserObject = { facebookID: socialID, firstName: firstName, lastName: lastName, accessToken: accessToken };
+                break;
+            case 'google':
+                socialObject = { googleID: socialID };
+                socialUserObject = { googleID: socialID, firstName: firstName, lastName: lastName, accessToken: accessToken };
+                break;
+            case 'twitter':
+                socialObject = { twitterID: socialID };
+                socialUserObject = { twitterID: socialID, firstName: firstName, lastName: lastName, accessToken: accessToken };
+                break;
+            case 'linkedin':
+                socialObject = { linkedInID: socialID };
+                socialUserObject = { linkedInID: socialID, firstName: firstName, lastName: lastName, accessToken: accessToken };
+                break;
+        }
+
         bcrypt.genSalt(10, function(err, salt) {
-            if (err) return next(err);
+            if (err) {
+                return next(err);
+            }
 
             bcrypt.hash(thingToEncrypt, salt, function() {} , function(err, hash) {
-                if (err) return next(err);
+                if (err) {
+                    return next(err);
+                }
                 accessToken = hash;
-                if (provider == "facebook") {
-                    Users.find().where({facebookID: socialID})
+                Users.find().where(socialObject)
                     .then( function (found) {
 
                         if (found.length >= 1) {
-                            Users.update({facebookID: socialID}, {accessToken: accessToken})
-                            .then(function (updated) {
-                                return res.json(updated);
-                            })
-                            .fail(function(err){
-                                return res.serverError(err);
-                            });
+                            Users.update(socialObject, {accessToken: accessToken})
+                                .then(function (updated) {
+                                    return res.json(updated);
+                                })
+                                .fail(function(err){
+                                    return res.serverError(err);
+                                });
 
-                        } else if (found == null || found == undefined || found.length < 1) {
-                            Users.create({facebookID: socialID, firstName: firstName, lastName: lastName, accessToken: accessToken})
-                            .then(function (created) {
-                                return res.json(created);
-                            })
-                            .fail(function(err){
-                                return res.serverError(err);
-                            });
+                        } else if (!found || found.length < 1) {
+                            Users.create(socialUserObject)
+                                .then(function (created) {
+                                    return res.json(created);
+                                })
+                                .fail(function(err){
+                                    return res.serverError(err);
+                                });
                         }
                     })
                     .fail(function(err){
                         return res.serverError(err);
                     });
-                } else if (provider == "google") {
-                    Users.find().where({googleID: socialID})
-                        .then( function (found) {
-
-                            if (found.length >= 1) {
-                                Users.update({googleID: socialID}, {accessToken: accessToken})
-                                    .then(function (updated) {
-                                        return res.json(updated);
-                                    })
-                                    .fail(function(err){
-                                        return res.serverError(err);
-                                    });
-
-                            } else if (found == null || found == undefined || found.length < 1) {
-                                Users.create({googleID: socialID, firstName: firstName, lastName: lastName, accessToken: accessToken})
-                                    .then(function (created) {
-                                        return res.json(created);
-                                    })
-                                    .fail(function(err){
-                                        return res.serverError(err);
-                                    });
-                            }
-                        })
-                        .fail(function(err){
-                            return res.serverError(err);
-                        });
-                } else if (provider == "twitter") {
-                    Users.find().where({twitterID: socialID})
-                        .then( function (found) {
-
-                            if (found.length >= 1) {
-                                Users.update({twitterID: socialID}, {accessToken: accessToken})
-                                    .then(function (updated) {
-                                        return res.json(updated);
-                                    })
-                                    .fail(function(err){
-                                        return res.serverError(err);
-                                    });
-
-                            } else if (found == null || found == undefined || found.length < 1) {
-                                Users.create({twitterID: socialID, firstName: firstName, lastName: lastName, accessToken: accessToken})
-                                    .then(function (created) {
-                                        return res.json(created);
-                                    })
-                                    .fail(function(err){
-                                        return res.serverError(err);
-                                    });
-                            }
-                        })
-                        .fail(function(err){
-                            return res.serverError(err);
-                        });
-                } else if (provider == "linkedin") {
-                    Users.find().where({linkedInID: socialID})
-                        .then( function (found) {
-
-                            if (found.length >= 1) {
-                                Users.update({linkedInID: socialID}, {accessToken: accessToken})
-                                    .then(function (updated) {
-                                        return res.json(updated);
-                                    })
-                                    .fail(function(err){
-                                        return res.serverError(err);
-                                    });
-
-                            } else if (found == null || found == undefined || found.length < 1) {
-                                Users.create({linkedInID: socialID, firstName: firstName, lastName: lastName, accessToken: accessToken})
-                                    .then(function (created) {
-                                        return res.json(created);
-                                    })
-                                    .fail(function(err){
-                                        return res.serverError(err);
-                                    });
-                            }
-                        })
-                        .fail(function(err){
-                            return res.serverError(err);
-                        });
-                }
             });
         });
     },
 
     linkSocialAccount: function (req, res) {
-        var provider = req.body.provider;
-        var socialID = req.body.socialID;
-        var id = req.body.id;
-        var token = req.body.token;
-        if (provider == "facebook") {
-            Users.update({id: id, accessToken:token}, { facebookID: socialID} )
-            .then(function (updated){
-                return res.json(updated);
-            })
-            .failed(function(err) {
-                return res.serverError(err);
-            });
-        } else if (provider == "google") {
-            Users.update({id: id, accessToken:token}, { googleID: socialID} )
-            .then(function (updated){
-                return res.json(updated);
-            })
-            .failed(function(err) {
-                return res.serverError(err);
-            });
-        } else if (provider == "twitter") {
-            Users.update({id: id, accessToken:token}, { twitterID: socialID} )
-            .then(function (updated){
-                return res.json(updated);
-            })
-            .failed(function(err) {
-                return res.serverError(err);
-            });
-        } else if (provider == "linkedin") {
-            Users.update({id: id, accessToken:token}, { linkedInID: socialID} )
-            .then(function (updated){
-                return res.json(updated);
-            })
-            .failed(function(err) {
-                return res.serverError(err);
-            });
+        var provider = req.body.provider,
+            socialID = req.body.socialID,
+            id = req.body.id,
+            token = req.body.token,
+            socialObject;
+
+        switch (provider){
+            case 'facebook':
+                socialObject = { facebookID: socialID };
+                break;
+            case 'google':
+                socialObject = { googleID: socialID };
+                break;
+            case 'twitter':
+                socialObject = { twitterID: socialID };
+                break;
+            case 'linkedin':
+                socialObject = { linkedInID: socialID };
+                break;
         }
+
+        Users.update({id: id, accessToken:token}, socialObject )
+            .then(function (updated){
+                return res.json(updated);
+            })
+            .failed(function(err) {
+                return res.serverError(err);
+            });
+
+
     }
+
 };
 
